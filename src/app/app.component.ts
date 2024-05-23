75ec27da4e260cf62dcb40e4fd75b28637eb670c
mvialo <manuella.vialo@outlook.fr>
2024-05-23 16:51:20 +0200

    Transmission du local a Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from './block/block.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Block';
+  title = "Mon site i sa bloque cet' zot !";
 }
